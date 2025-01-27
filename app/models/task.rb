e4f4fc30bacfd77088a70105f37c2756f0ec082d
RuslanHamidov <rhamidov202@gmail.com>
2025-01-27 18:10:23 +0400

    add data validation

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,2 +1,12 @@
 class Task < ApplicationRecord
+  validates :title, presence: true, length: { maximum: 15}
+  validates :description, length: { in: 6..100 }, allow_blank: true
+
+  after_initialize :set_default_status, if: :new_record?
+
+  private
+
+  def set_default_status
+    self.status ||= false
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2025_01_27_075053) do
+ActiveRecord::Schema[7.1].define(version: 2025_01_27_140930) do
   create_table "tasks", force: :cascade do |t|
     t.string "title"
     t.string "description"
-    t.boolean "status"
+    t.boolean "status", default: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
